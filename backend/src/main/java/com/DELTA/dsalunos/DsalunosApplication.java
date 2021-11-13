6a243271137e1be1126c3b75fac27c9aadbfb855
jajunio333 <jajunio333@gmail.com>
2021-11-13 10:39:06 -0300

    Projeto criado

---
+++ backend/src/main/java/com/DELTA/dsalunos/DsalunosApplication.java
@@ -0,0 +1,13 @@
+package com.DELTA.dsalunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsalunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsalunosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/DELTA/dsalunos/DsalunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DELTA.dsalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsalunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
