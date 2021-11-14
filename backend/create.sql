1259fbcd62641ec59af4fd19db15080bff19b0c6
jajunio333 <jajunio333@gmail.com>
2021-11-14 10:47:37 -0300

    configurando os ambientes

---
+++ backend/create.sql
@@ -0,0 +1,10 @@
+create sequence hibernate_sequence start 1 increment 1;
+create table tb_alunos (id int8 not null, endereço varchar(255), foto varchar(255), nome varchar(255), primary key (id));
+create sequence hibernate_sequence start 1 increment 1;
+create table tb_alunos (id int8 not null, endereço varchar(255), foto varchar(255), nome varchar(255), primary key (id));
+create sequence hibernate_sequence start 1 increment 1;
+create table tb_alunos (id int8 not null, endereço varchar(255), foto varchar(255), nome varchar(255), primary key (id));
+create sequence hibernate_sequence start 1 increment 1;
+create table tb_alunos (id int8 not null, endereço varchar(255), foto varchar(255), nome varchar(255), primary key (id));
+create sequence hibernate_sequence start 1 increment 1;
+create table tb_alunos (id int8 not null, endereço varchar(255), foto varchar(255), nome varchar(255), primary key (id));
